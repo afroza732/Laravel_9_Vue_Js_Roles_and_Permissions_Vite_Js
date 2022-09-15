8442ddcc45da91c35fcdebecb19294be8baf5cd9
nira19972 <nira19972@gmail.com>
2022-09-15 13:18:38 +0600

    user todo complete with vue and larevl breeze

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-
+use Illuminate\Support\Facades\Gate;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -24,7 +24,11 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
+        Gate::before(function ($user, $ability) {
+            if ($user->hasRole(env('APP_SUPER_ADMIN', 'super-admin'))) {
+                return true;
+            }
+        });
         //
     }
 }
